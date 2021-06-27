7211fc0f45163a2714b27824021270ff578438be
Vinodharajan Kanagarajan <vinodharajan@gmail.com>
2021-06-27 06:56:50 +0530

    Initial Commit

---
+++ src/Modules/CSM.Partners.Core/Startup.cs
@@ -0,0 +1,25 @@
+using System;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Routing;
+using Microsoft.Extensions.DependencyInjection;
+using OrchardCore.Modules;
+
+namespace CSM.Partners.Core
+{
+    public class Startup : StartupBase
+    {
+        public override void ConfigureServices(IServiceCollection services)
+        {
+        }
+
+        public override void Configure(IApplicationBuilder builder, IEndpointRouteBuilder routes, IServiceProvider serviceProvider)
+        {
+            routes.MapAreaControllerRoute(
+                name: "Home",
+                areaName: "CSM.Partners.Core",
+                pattern: "Home/Index",
+                defaults: new { controller = "Home", action = "Index" }
+            );
+        }
+    }
+}
